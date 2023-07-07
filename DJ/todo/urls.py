b5464fa64b6f0ec603565870090a941382bc659d
Ramakrishnan-D <krishdev94@gmail.com>
2023-07-07 16:25:40 +0530

    url and looping

--- DJ/todo/urls.py
+++ DJ/todo/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('about/',views.about),
-    path('work/',views.work),
+    path('',views.home,name="home"),
+    path('about/',views.about,name="about"),
+    path('work/',views.work,name="work"),
+    path('about/view/<str:uname>',views.viewUser,name="info"),
 
 ]
--- DJ/todo/views.py
+++ DJ/todo/views.py
@@ -4,10 +4,28 @@ from django.shortcuts import render,HttpResponse
 def home(req):
     return render(req,'home.html',{"name":"python"})
 
-
+users = [
+        {"name":"python","age":20,"email":"py20@gmail.com"},
+        {"name":"java","age":20,"email":"java@gmail.com"},
+        {"name":"C++","age":20,"email":"CPP@gmail.com"},
+        {"name":"dart","age":20,"email":"dart@gmail.com"},
+        {"name":"flutter","age":20,"email":"flutter@gmail.com"},
+        ]
 def about(req):
-    return render(req,'about.html',{"isDone":'z'})
+   
+    return render(req,'about.html',{"isDone":'z',"users":users})
 
 def work(req):
-    return render(req,'work.html')
+    return render(req,'work.html',{"data":["s",1,2,3,4]})
+
+
+def viewUser(req,uname):
+    print(uname)
+    ud = {}
+    for i in users:
+        if(i['name'] == uname):
+            ud = i
+            break
+    return render(req,'info.html',{"info":ud})
+
 
